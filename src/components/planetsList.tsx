c43fe7d82fc96499633c3ad79986e5f7f411efb8
Marcin Stepien <ms@mmlab.ie>
2021-07-12 11:27:45 +1000

    created two reducers for charaters and planets

--- src/components/planetsList.tsx
+++ src/components/planetsList.tsx
@@ -1,9 +1,31 @@
-const planetList: React.FC = () => {
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { actionCreators, State } from "../state";
+
+const PlanetList: React.FC = () => {
+  const dispatch = useDispatch();
+  const { data } = useSelector((state: State) => state.planets);
+
+  useEffect(() => {
+    dispatch(actionCreators.fetchPlanets());
+  }, [dispatch]);
+
+  const renderPlanetsList = () => {
+    return (
+      <ul>
+        {data.map((planet: string) => {
+          return <li key={planet}>{planet}</li>;
+        })}
+      </ul>
+    );
+  };
+
   return (
     <div>
       <h3>Planets:</h3>
+      {renderPlanetsList()}
     </div>
   );
 };
 
-export default planetList;
+export default PlanetList;
